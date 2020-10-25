8eddda169022efb007f0d9c66fe14795b85510c6
Ana Maria Totorean <anamaria.totorean@3pillarglobal.com>
2020-10-26 00:25:10 +0200

    Add more theme colors and reset font size to 10px globally

--- src/style/globalStyle.js
+++ src/style/globalStyle.js
@@ -9,6 +9,17 @@ const GlobalStyle = createGlobalStyle`
                  'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
                  sans-serif;
   }
+  html, body, div, span, applet, object, iframe,
+  h1, h2, h3, h4, h5, h6, p, blockquote, pre, a,
+  abbr, acronym, address, big, cite, code, del, dfn,
+  em, img, ins, kbd, q, s, samp, small, strike, strong,
+  sub, sup, tt, var, b, u, i, center, dl, dt, dd, ol, ul,
+  li, fieldset, form, label, legend, table, caption, tbody,
+  tfoot, thead, tr, th, td, article, aside, canvas, details,
+  embed, figure, figcaption, footer, header, hgroup, main,
+  menu, nav, output, ruby, section, summary, time, mark, audio, video {
+    font-size: 10px;
+  }
 `;
 
 export default GlobalStyle;
--- src/style/globalTheme.js
+++ src/style/globalTheme.js
@@ -1,7 +1,8 @@
 // eslint-disable-next-line import/prefer-default-export
 export const theme = {
     bgBody: '#F7F7F7',
-    bgHeader: '#FFFFFF',
     textColor: '#121212',
     labelColor: '#727272',
+    bgHeader: '#FFFFFF',
+    borderHeader: '#EBEBEB',
 };
